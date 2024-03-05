c898a7b14c77448d9f715da570785f97db9639ba
zhengyongkai <825947557@qq.com>
2024-03-05 22:53:38 +0800

    ✨ feat(page):
    1. 样式大改

--- .history/src/components/Markdown/index_20240305215707.jsx
+++ .history/src/components/Markdown/index_20240305215707.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "codemirror/lib/codemirror.css";
-
+import "@toast-ui/editor/dist/toastui-editor.css";
 import { Editor } from "@toast-ui/react-editor";
 const Markdown = () => {
   return (
